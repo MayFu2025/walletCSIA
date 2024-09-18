ec63ab349e106ddf1f9152c540a546e2165ea1a7
May <2025.may.fujita@uwcisak.jp>
2024-09-19 02:17:16 +0900

    save2

--- walletCSIA/view/setting/settings.swift
+++ walletCSIA/view/setting/settings.swift
@@ -6,13 +6,34 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct settings: View {
+    var userName: String?
+    @Query var categories: [Category]
+    @Query var currencies: [Currency]
+    @Query var expenses: [Expense]
+    
     var body: some View {
         NavigationStack{
             List{
                 Section(header: Text("User")) {
-                    Text("Test text")
+                    Text("Bernard Lee")
+                    Text("Add toggle here to enable password/FaceID")
+                }
+                Section(header: Text("All Instances")) {
+                    NavigationLink(destination: graphScreen()) {
+                        Text("Categories")
+                    }
+                    NavigationLink(destination: graphScreen()) {
+                        Text("Currencies")
+                    }
+                    NavigationLink(destination: graphScreen()) {
+                        Text("Expenses")
+                    }
+                }
+                Section(header: Text("Reset")) {
+                    Text("Button with confirm popup")
                 }
             }
             Text("This is settings screen")
