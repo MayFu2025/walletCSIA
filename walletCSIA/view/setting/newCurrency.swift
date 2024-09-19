c473c0348fa1e3028d71c52de47e77dadbc4ea43
May <2025.may.fujita@uwcisak.jp>
2024-09-19 12:47:04 +0900

    Add input validation for currency code

---
+++ walletCSIA/view/setting/newCurrency.swift
@@ -0,0 +1,18 @@
+//
+//  newCurrency.swift
+//  walletCSIA
+//
+//  Created by May Fujita on 2024/09/19.
+//
+
+import SwiftUI
+
+struct newCurrency: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    newCurrency()
+}
