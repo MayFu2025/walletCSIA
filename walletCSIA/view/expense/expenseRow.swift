b9809463698b678f62357a6d3ce47cee2fd6a61c
May <2025.may.fujita@uwcisak.jp>
2024-09-23 03:09:22 +0900

    haha muri

---
+++ walletCSIA/view/expense/expenseRow.swift
@@ -0,0 +1,18 @@
+//
+//  expenseRow.swift
+//  walletCSIA
+//
+//  Created by May Fujita on 2024/09/23.
+//
+
+import SwiftUI
+
+struct expenseRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    expenseRow()
+}
